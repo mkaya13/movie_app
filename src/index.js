104808ec8711da4ef2bcc6219970c788580c9cdd
SekoVper <damehlivingstone1@gmail.com>
2022-11-22 14:36:36 +0100

    Call method for page navigations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { getMovies } from '../modules/getMovies.js';
 import { show } from '../modules/movies.js';
+import { showComponent } from '../modules/nav.js';
 import './style.css';
 
 getMovies();
 show();
+showComponent();
