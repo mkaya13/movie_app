b70304d7232ebe16146299f8ab01be2198338519
SekoVper <damehlivingstone1@gmail.com>
2022-11-24 13:11:50 +0100

    Add like button and interactions

--- modules/counter.js
+++ modules/counter.js
@@ -2,32 +2,30 @@ import { getLikes } from './getLikes.js';
 
 const APIUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/nmfrG36tREDV4HGApbkk/likes';
 
-const createLike = async () => {
+const createLike = async (itemId) => {
   await fetch(APIUrl, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ item_id: '' }),
+    body: JSON.stringify({ item_id: itemId }),
   });
-};
-
-export const retrieveLikes = async (index) => {
-  const data = await getLikes();
-  const likesVal = parseInt(data[index].likes, 10);
-  if (likesVal >= 0) {
-    console.log(likesVal + 1);
-  } else {
-    console.log(likesVal - 1);
-  }
 };
 
 export const counter = () => {
   const likeBtn = document.querySelectorAll('.favorite');
-  likeBtn.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-      createLike();
-      retrieveLikes(index);
+  likeBtn.forEach((btn, index, itemId) => {
+    btn.addEventListener('click', async () => {
+      if (itemId) {
+        btn.classList.toggle('favorite-color');
+      }
+      const data = await getLikes();
+      itemId = data[index].item_id;
+      const likesCount = data[index].likes;
+      if (likesCount) {
+        createLike(itemId);
+        getLikes();
+      }
     });
   });
 };
 
-export default { counter, retrieveLikes };
+export default { counter };
